2d9b5b8c23a77424b27026d30ccfb27e68ffa4e5
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2024-01-10 17:15:24 -0300

    redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/": "/promo",
+    "/polgo": "/promo",
+  },
+});
